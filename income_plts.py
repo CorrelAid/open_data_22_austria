9d444f0923cc6abab464a776221c8932e3a747a5
Tilman Kerl <tilman.kerl@uni-konstanz.de>
2022-03-05 15:38:30 +0100

    add dog _ dogbeutel

--- income_plts.py
+++ income_plts.py
@@ -10,15 +10,24 @@ income = income[["bezirk", 'avg_income_fulltime_total', 'avg_income_fulltime_men
 income = income[["bezirk", 'avg_income_fulltime_total']]
 
 income.plot.bar(x="bezirk")
-plt.savefig("income_bar.png")
+plt.savefig("plots/income_bar.png")
 
 income.groupby(['bezirk']).sum().plot(kind='pie', y='avg_income_fulltime_total')
-plt.savefig("income_pie.png")
+plt.savefig("plots/income_pie.png")
 
-dogs = pd.read_csv("data/hunde/hundebeutel_pro_bezirk.csv", sep=",")
-mdf = dogs.merge(income)
+dog_bag = pd.read_csv("data/hunde/hundebeutel_pro_bezirk.csv", sep=",")
+mdf = dog_bag.merge(income)
 
 sns.lmplot(x="summe_beutel_bezirk", y="avg_income_fulltime_total",data=mdf,fit_reg=True) 
 # mdf.plot.scatter(x="summe_beutel_bezirk", y="avg_income_fulltime_total", c="DarkBlue")
-plt.savefig("hundebeutel_income.png")
+plt.savefig("plots/hundebeutel_income.png")
 print(mdf)
+
+
+dogs = pd.read_csv("data/hunde/hunde_pro_bezirk_2020.csv")
+ddog = dogs.merge(dog_bag)
+ddog = ddog[["bezirk", "summe_beutel_bezirk", "dog_density"]]
+sns.lmplot(x="summe_beutel_bezirk", y="dog_density",data=ddog,fit_reg=True) 
+# mdf.plot.scatter(x="summe_beutel_bezirk", y="avg_income_fulltime_total", c="DarkBlue")
+plt.savefig("plots/hundebeutel_hunde.png")
+print(ddog)
