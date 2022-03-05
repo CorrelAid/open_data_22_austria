7a314e06b5114b72fbae328f9d62dcf5d26cac44
Tilman Kerl <tilman.kerl@uni-konstanz.de>
2022-03-05 15:32:22 +0100

    move plots + income & hundebeutel

--- income_plts.py
+++ income_plts.py
@@ -1,5 +1,6 @@
 
 import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 
 income = pd.read_csv("data/income/income.csv", sep=",")
@@ -13,3 +14,11 @@ plt.savefig("income_bar.png")
 
 income.groupby(['bezirk']).sum().plot(kind='pie', y='avg_income_fulltime_total')
 plt.savefig("income_pie.png")
+
+dogs = pd.read_csv("data/hunde/hundebeutel_pro_bezirk.csv", sep=",")
+mdf = dogs.merge(income)
+
+sns.lmplot(x="summe_beutel_bezirk", y="avg_income_fulltime_total",data=mdf,fit_reg=True) 
+# mdf.plot.scatter(x="summe_beutel_bezirk", y="avg_income_fulltime_total", c="DarkBlue")
+plt.savefig("hundebeutel_income.png")
+print(mdf)
