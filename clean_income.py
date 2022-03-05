4f853cf12119a006fb3a428dfeda9334bd68a64e
Tilman Kerl <tilman.kerl@uni-konstanz.de>
2022-03-05 14:03:09 +0100

    final clean steps

--- clean_income.py
+++ clean_income.py
@@ -3,11 +3,17 @@ from calendar import c
 import pandas as pd
 
 DROP_COLS = ["SUB_DISTRICT_CODE", "INCOME_MEN", "INCOME_WOMEN", "INCOME_TOTAL", "REF_DATE"]
-income = pd.read_csv("input/data/vie_502.csv", sep=";")
+income = pd.read_csv("data/raw/vie_502.csv", sep=";")
 income = income.query("REF_DATE == 20181231")
 income.drop([f"NUTS{x+1}" for x in range(3)] + DROP_COLS, axis=1, inplace=True)
 income.rename(columns = {"DISTRICT_CODE": "bezirk"}, inplace=True)  
 income.bezirk = income.bezirk.apply(lambda x: int(str(x)[1:3]))
+
+income.columns = income.columns.str.lower()
+income.columns = income.columns.str.replace('average', 'avg')
 print(income)
+
+for col in income.columns:
+    income[col].astype(int)
 
 income.to_csv("data/income.csv", index=False)
