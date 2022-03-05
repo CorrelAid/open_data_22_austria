90755a45d2328a93059e6497c3244039d17d3225
Tilman Kerl <tilman.kerl@uni-konstanz.de>
2022-03-05 14:05:22 +0100

    jz aber wirklich final clean income

--- clean_income.py
+++ clean_income.py
@@ -11,9 +11,11 @@ income.bezirk = income.bezirk.apply(lambda x: int(str(x)[1:3]))
 
 income.columns = income.columns.str.lower()
 income.columns = income.columns.str.replace('average', 'avg')
-print(income)
 
 for col in income.columns:
     income[col].astype(int)
+
+income = income.reset_index()
+print(income)
 
 income.to_csv("data/income.csv", index=False)
