b69023aff898f5b961d50e551285fd5901321a0b
Tilman Kerl <tilman.kerl@uni-konstanz.de>
2022-03-05 13:37:58 +0100

    readme & clean data & restruct a little bit

--- clean_income.py
+++ clean_income.py
@@ -1,8 +1,13 @@
 
+from calendar import c
 import pandas as pd
 
-income = pd.read_csv("data/vie_502.csv", sep=";")
-print(income)
+DROP_COLS = ["SUB_DISTRICT_CODE", "INCOME_MEN", "INCOME_WOMEN", "INCOME_TOTAL", "REF_DATE"]
+income = pd.read_csv("input/data/vie_502.csv", sep=";")
 income = income.query("REF_DATE == 20181231")
-income.DISTRICT_CODE.str.replace("")
+income.drop([f"NUTS{x+1}" for x in range(3)] + DROP_COLS, axis=1, inplace=True)
+income.rename(columns = {"DISTRICT_CODE": "bezirk"}, inplace=True)  
+income.bezirk = income.bezirk.apply(lambda x: int(str(x)[1:3]))
 print(income)
+
+income.to_csv("data/income.csv", index=False)
